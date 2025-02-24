059099826121e7e9c84925cc92fa9ead4c0da5b4
Marlen Brunner <klondikemarlen@gmail.com>
2025-02-23 22:19:43 -0700

    :heavy_check_mark: Add tests for each basic api action.

--- tests/base-controller/api.test.ts
+++ tests/base-controller/api.test.ts
@@ -8,10 +8,62 @@ describe("src/base-controller/api.ts", () => {
     const app = express()
 
     describe(".index", () => {
-      app.route("/base-api").get(API.index)
+      app.route("/example-api").get(API.index)
 
       test("when called, executes the index action", async () => {
-        const response = await request(app).get("/base-api").send()
+        const response = await request(app).get("/example-api").send()
+
+        expect(response.status).toBe(500)
+        expect(response.body).toMatchObject({
+          message: "Not Implemented",
+        })
+      })
+    })
+
+    describe(".create", () => {
+      app.route("/example-api").post(API.create)
+
+      test("when called, executes the create action", async () => {
+        const response = await request(app).post("/example-api").send()
+
+        expect(response.status).toBe(500)
+        expect(response.body).toMatchObject({
+          message: "Not Implemented",
+        })
+      })
+    })
+
+    describe(".show", () => {
+      app.route("/example-api/:someParam").get(API.show)
+
+      test("when called, executes the show action", async () => {
+        const response = await request(app).get("/example-api/1").send()
+
+        expect(response.status).toBe(500)
+        expect(response.body).toMatchObject({
+          message: "Not Implemented",
+        })
+      })
+    })
+
+    describe(".update", () => {
+      app.route("/example-api/:someParam").put(API.update)
+
+      test("when called, executes the update action", async () => {
+        const response = await request(app).put("/example-api/1").send()
+
+        expect(response.status).toBe(500)
+        expect(response.body).toMatchObject({
+          message: "Not Implemented",
+        })
+      })
+    })
+
+    describe(".destroy", () => {
+      app.route("/example-api/:someParam").delete(API.destroy)
+
+      test("when called, executes the destroy action", async () => {
+        const response = await request(app).delete("/example-api/1").send()
 
         expect(response.status).toBe(500)
         expect(response.body).toMatchObject({
