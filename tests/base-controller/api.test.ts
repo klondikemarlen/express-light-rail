0e1f8a5b1779c67b48b84eed24b2486353604fe8
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-06 17:11:57 -0700

    :japanese_castle: Use better isolation between api tests.

--- tests/base-controller/api.test.ts
+++ tests/base-controller/api.test.ts
@@ -5,10 +5,16 @@ import API, { BaseApiError } from "@/base-controller/api.js"
 
 describe("src/base-controller/api.ts", () => {
   describe("API", () => {
-    const app = express()
+    let app: express.Application
+
+    beforeEach(() => {
+      app = express()
+    })
 
     describe(".index", () => {
-      app.route("/example-api").get(API.index)
+      beforeEach(() => {
+        app.route("/example-api").get(API.index)
+      })
 
       test("when called, executes the index action", async () => {
         const response = await request(app).get("/example-api").send()
@@ -21,7 +27,9 @@ describe("src/base-controller/api.ts", () => {
     })
 
     describe(".create", () => {
-      app.route("/example-api").post(API.create)
+      beforeEach(() => {
+        app.route("/example-api").post(API.create)
+      })
 
       test("when called, executes the create action", async () => {
         const response = await request(app).post("/example-api").send()
@@ -34,7 +42,9 @@ describe("src/base-controller/api.ts", () => {
     })
 
     describe(".show", () => {
-      app.route("/example-api/:someParam").get(API.show)
+      beforeEach(() => {
+        app.route("/example-api/:someParam").get(API.show)
+      })
 
       test("when called, executes the show action", async () => {
         const response = await request(app).get("/example-api/1").send()
@@ -47,7 +57,9 @@ describe("src/base-controller/api.ts", () => {
     })
 
     describe(".update", () => {
-      app.route("/example-api/:someParam").put(API.update)
+      beforeEach(() => {
+        app.route("/example-api/:someParam").put(API.update)
+      })
 
       test("when called, executes the update action", async () => {
         const response = await request(app).put("/example-api/1").send()
@@ -60,7 +72,9 @@ describe("src/base-controller/api.ts", () => {
     })
 
     describe(".destroy", () => {
-      app.route("/example-api/:someParam").delete(API.destroy)
+      beforeEach(() => {
+        app.route("/example-api/:someParam").delete(API.destroy)
+      })
 
       test("when called, executes the destroy action", async () => {
         const response = await request(app).delete("/example-api/1").send()
@@ -88,10 +102,12 @@ describe("src/base-controller/api.ts", () => {
         }
       }
 
-      app.route("/example-error-api").post(ExampleController.create)
+      beforeEach(() => {
+        app.route("/example-api").post(ExampleController.create)
+      })
 
       test("when action errors with an known API error, returns the error", async () => {
-        const response = await request(app).post("/example-error-api").send()
+        const response = await request(app).post("/example-api").send()
 
         expect(response.status).toBe(400)
         expect(response.body).toMatchObject({
