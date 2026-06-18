fb8e89b1932201874570342315aef897b4850f69
Marlen Brunner <klondikemarlen@gmail.com>
2026-06-18 11:58:26 -0700

    :hammer: Build package artifacts with Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,32 @@
-import { defineConfig } from "vitest/config"
+/// <reference types="vitest/config" />
+
+import { resolve } from "node:path"
+
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+const externalDependencies = ["@sequelize/core", "express", "lodash", "winston"]
+
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ExpressLightRail",
+      fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: externalDependencies,
+    },
+  },
   plugins: [
     tsconfigPaths({
       root: ".",
       projects: ["./tsconfig.json", "./tests/tsconfig.json"],
+    }),
+    dts({
+      include: ["src/**/*.ts"],
     }),
   ],
   test: {
